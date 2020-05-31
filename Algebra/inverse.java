e0c4ce321d4fd2231cd85e071953e2ecfdacc31c
Cheongsol <63346007+Cheongsol@users.noreply.github.com>
2020-06-01 00:33:55 +0900

    Refactoring Algebra
    중복되는 메서드 제거. 더 자세한 메서드를 유지

--- Algebra/inverse.java
+++ Algebra/inverse.java
@@ -1,9 +1,26 @@
+package Algebra;
 import java.util.Scanner;
 
-public class inverse {
-	
+public class Inverse {
+	static int userChoice;
+	static double value;
+	static String s;
 	public static void main(String[] args){
+		new Inverse().prepare();
+		double valueRadians = Math.toRadians(value);
+		double result = 0;		
+		switch(userChoice){
 		
+		case 1: result = cosec(valueRadians); break;
+		case 2: result = sec(valueRadians);   break;
+		case 3: result = cot(valueRadians);   break;
+		
+		default: System.out.println("Invalid Choice. \n Exiting!");
+		}
+		System.out.println( s + " is: " + result);
+	}
+	
+	public void prepare() {
 		System.out.println("Welcome to the inverse trigonometry calculator!");
 		Scanner scan = new Scanner(System.in);
 		
@@ -12,100 +29,48 @@ public class inverse {
 				+ "\n 1 Cosecant"
 				+ "\n 2 Secant"
 				+ "\n 3 Cotangent\n");
-		
-		int userChoice = scan.nextInt();
-		
-		switch(userChoice){
-		
-		case 1: cosec(); break;
-		case 2: sec();   break;
-		case 3: cot();   break;
-		
-		
-		default: System.out.println("Invalid Choice. \n Exiting!");
-		}
-		
-	}
-
-	public static double cosec(){
-		
+		userChoice = scan.nextInt();
 		//Ask for user input
 		System.out.print("Enter an angle in degrees: ");
-		
 		//use scanner to read the console input 
-		Scanner scan = new Scanner(System.in);
-		
+		scan = new Scanner(System.in);
 		//Assign the user to String variable
-		String s = scan.nextLine();
-		
+		s = scan.nextLine();
 		//close the scanner object
 		scan.close();
-		
 		//convert the string input to double
-		double value = Double.parseDouble(s);
+		value = Double.parseDouble(s);
 		//convert the value to radians
-		double valueRadians = Math.toRadians(value);
+	}
+
+	public static double cosec(double valueRadians){
 		
 		Math.sin(valueRadians);
 		
 		//get the cosec of the angle
 		double cosecValue = 1.0/Math.sin(valueRadians);
-		System.out.println("cosec of " + s + " is: " + cosecValue);
+		System.out.println("cosec of ");
 		return cosecValue;
 		
 	
 	}
-public static double sec(){
-	
-	//Ask for user input
-			System.out.print("Enter an angle in degrees: ");
-			
-			//use scanner to read the console input 
-			Scanner scan = new Scanner(System.in);
-			
-			//Assign the user to String variable
-			String s = scan.nextLine();
-			
-			//close the scanner object
-			scan.close();
-			
-			//convert the string input to double
-			double value = Double.parseDouble(s);
-			//convert the value to radians
-			double valueRadians = Math.toRadians(value);
-			
-			Math.cos(valueRadians);
-			
-			//get the sec of the angle
-			double secValue = 1.0/Math.cos(valueRadians);
-			System.out.println("sec of " + s + " is: " + secValue);
-			return secValue;
-}
+	public static double sec(double valueRadians){
+				
+		Math.cos(valueRadians);
+		
+		//get the sec of the angle
+		double secValue = 1.0/Math.cos(valueRadians);
+		System.out.println("sec of ");
+		return secValue;
+	}
 
-public static double cot(){
-	
-	//Ask for user input
-			System.out.print("Enter an angle in degrees: ");
+	public static double cot(double valueRadians){
 			
-			//use scanner to read the console input 
-			Scanner scan = new Scanner(System.in);
-			
-			//Assign the user to String variable
-			String s = scan.nextLine();
-			
-			//close the scanner object
-			scan.close();
-			
-			//convert the string input to double
-			double value = Double.parseDouble(s);
-			//convert the value to radians
-			double valueRadians = Math.toRadians(value);
-			
-			Math.tan(valueRadians);
-			
-			//get the cot of the angle
-			double cotValue = 1.0/Math.tan(valueRadians);
-			System.out.println("cot of " + s + " is: " + cotValue);
-			return cotValue;
-}
+		Math.tan(valueRadians);
+		
+		//get the cot of the angle
+		double cotValue = 1.0/Math.tan(valueRadians);
+		System.out.println("cot of ");
+		return cotValue;
+	}
 }
